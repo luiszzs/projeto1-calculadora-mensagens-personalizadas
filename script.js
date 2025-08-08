b88bb3046515bbbcc8ff9b29be4515635261539c
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2025-08-08 14:47:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let nome = "Luis Guilherme"
+let idade = 15
+let city = "General Sampaio"
+let Profissao = "Estudante"
+let anoatual = 2025
+let salario = 200
+let bonus = 100
+//calculos
+let salabonus = salario + bonus
+const anonascimento = anoatual - idade
+let totalano = salabonus * 12
+let dobrototalano = totalano * 2
+alert(`
+    =========Relatório do usuário=========
+    Nome: ${nome}
+     Idade: ${idade}
+     Ano de nascimento: ${anonascimento}
+     Cidade: ${city}
+     Profissão: ${Profissao}    
+    
+     Sálario base: U$${salario}
+     Bônus: U$${bonus}
+     Total Mensal: U$${salabonus} 
+     Total ano: U$${totalano}
+     Total anual: U$${dobrototalano}
+    =====================================
+    `)
