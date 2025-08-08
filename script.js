5073348dd738b7f8bfefbe3b11f1c713a73953a8
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2025-08-08 14:22:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let salario = 200
 let bonus = 100
 //calculos
 let salabonus = salario + bonus
-let anonascimento = anoatual - idade
+const anonascimento = anoatual - idade
 let totalano = salabonus * 12
 let dobrototalano = totalano * 2
 alert(`
