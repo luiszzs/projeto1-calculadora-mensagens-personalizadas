c9d513a6e28e5ef11667f9393c978302873e73bd
luiszzs <luis.gomes76@aluno.ce.gov.br>
2025-08-07 22:51:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const nome = "Luis Guilherme"
+let idade = 15
+let city = "General Sampaio"
+let Profissao = "Estudante"
+let anoatual = 2025
+let salario = 200
+let bonus = 100
+let salabonus = (salario) + (bonus)
+let anoto
+alert(`
+    =========Relatório do usuário=========
+    Nome: ${nome}
+     Idade: ${idade}
+     Ano de nascimento: ${anoatual - idade}
+     Cidade: ${city}
+     Profissão: ${Profissao}    
+    
+     Sálario base: U$${salario}
+     Bônus: U$${bonus}
+     Total Mensal: U$${salario + bonus}    
+     Total ano: U$${ salabonus * 12}
+     Total anual: U$${salabonus * 12 * 2}    
+    =====================================
+    
+    `)
